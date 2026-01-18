17d9ff0646a3cb345c7f50e67f4fe52e42614277
Maryana Golik <golikmaryana307@gmail.com>
2026-01-18 16:48:49 +0200

    Fix HttpError status handling

--- src/middleware/errorHandler.js
+++ src/middleware/errorHandler.js
@@ -2,13 +2,12 @@ import { HttpError } from 'http-errors';
 
 export const errorHandler = (err, req, res, next) => {
   if (err instanceof HttpError) {
-    return res.status(err.statusCode).json({
+    return res.status(err.status).json({
       message: err.message,
     });
   }
 
-  res.status(500).json({
-    message: 'Internal server error',
-  });
+  res.status(500).json({ message: 'Internal server error' });
 };
+
 

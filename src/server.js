5207c99f0ecbeb26de697f356d7f12a5fb710ccf
Maryana Golik <golikmaryana307@gmail.com>
2026-01-23 23:03:59 +0200

    Add celebrate errors middleware

--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import cors from 'cors';
 import 'dotenv/config';
+import { errors } from 'celebrate';
 
 import { connectMongoDB } from './db/connectMongoDB.js';
 import { logger } from './middleware/logger.js';
@@ -16,8 +17,7 @@ app.use(express.json());
 app.use(cors());
 
 app.use(notesRouter);
-
-
+app.use(errors());
 app.use(notFoundHandler);
 app.use(errorHandler);
 
@@ -30,6 +30,7 @@ const startServer = async () => {
 };
 
 startServer();
+
 
 
 

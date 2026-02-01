69fe9b13ac856be89df6e6217ce1b432efc7b204
Maryana Golik <golikmaryana307@gmail.com>
2026-02-01 23:11:18 +0200

    fix getAllNotes to use mongoose chaining

--- src/controllers/notesController.js
+++ src/controllers/notesController.js
@@ -1,56 +1,38 @@
 import createHttpError from "http-errors";
-import { Note } from "../models/note.js";
+import Note from "../models/note.model.js";
 
 export const getAllNotes = async (req, res) => {
-  const { page = 1, perPage = 10, tag, search } = req.query;
-  const skip = (page - 1) * perPage;
+  const { page = 1, limit = 10, tags, search } = req.query;
+  const skip = (page - 1) * limit;
 
-  let notesQuery = Note.find()
-    .where("userId")
-    .equals(req.user._id);
+  let query = Note.find().where("userId").equals(req.user._id);
 
-  let totalQuery = Note.countDocuments()
-    .where("userId")
-    .equals(req.user._id);
-
-  if (tag) {
-    notesQuery = notesQuery.where("tag").equals(tag);
-    totalQuery = totalQuery.where("tag").equals(tag);
+  if (tags) {
+    query = query.where("tags").in(tags.split(","));
   }
 
   if (search) {
-    const regex = new RegExp(search, "i");
-    notesQuery = notesQuery.where("title").regex(regex);
-    totalQuery = totalQuery.where("title").regex(regex);
+    query = query.find({ $text: { $search: search } });
   }
 
   const [notes, total] = await Promise.all([
-    notesQuery.skip(skip).limit(perPage),
-    totalQuery,
+    query.skip(skip).limit(limit).exec(),
+    Note.countDocuments(query.getQuery())
   ]);
 
-  const totalPages = Math.ceil(total / perPage);
-  if (page > totalPages && totalPages !== 0) {
-    throw createHttpError(400, "Page number exceeds total pages");
-  }
-
-  res.json({
-    page: Number(page),
-    perPage: Number(perPage),
-    totalNotes: total,
-    totalPages,
+  res.status(200).json({
     notes,
+    total,
+    page: Number(page),
+    limit: Number(limit)
   });
 };
 
 export const getNoteById = async (req, res) => {
-  const { noteId } = req.params;
-
-  const note = await Note.findOne()
-    .where("_id")
-    .equals(noteId)
-    .where("userId")
-    .equals(req.user._id);
+  const note = await Note.findOne({
+    _id: req.params.id,
+    userId: req.user._id
+  });
 
   if (!note) {
     throw createHttpError(404, "Note not found");
@@ -62,42 +44,39 @@ export const getNoteById = async (req, res) => {
 export const createNote = async (req, res) => {
   const note = await Note.create({
     ...req.body,
-    userId: req.user._id,
+    userId: req.user._id
   });
 
   res.status(201).json(note);
 };
 
-export const deleteNote = async (req, res, next) => {
-  const { noteId } = req.params;
-
-  const note = await Note.findOneAndDelete({
-    _id: noteId,
-    userId: req.user._id,
-  });
-
-  if (!note) {
-    return next(createHttpError(404, "Note not found"));
-  }
-
-  res.status(200).json(note);
-};
-
-export const updateNote = async (req, res, next) => {
-  const { noteId } = req.params;
-
+export const updateNote = async (req, res) => {
   const note = await Note.findOneAndUpdate(
-    { _id: noteId, userId: req.user._id },
+    { _id: req.params.id, userId: req.user._id },
     req.body,
     { new: true }
   );
 
   if (!note) {
-    return next(createHttpError(404, "Note not found"));
+    throw createHttpError(404, "Note not found");
   }
 
   res.status(200).json(note);
 };
+
+export const deleteNote = async (req, res) => {
+  const note = await Note.findOneAndDelete({
+    _id: req.params.id,
+    userId: req.user._id
+  });
+
+  if (!note) {
+    throw createHttpError(404, "Note not found");
+  }
+
+  res.status(204).send();
+};
+
 
 
 

f08055b7a2a981eeebc94e40e21b8b181ab93563
Maryana Golik <golikmaryana307@gmail.com>
2026-02-01 22:49:09 +0200

    fin 04-auth

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,17 +1,16 @@
 import createHttpError from "http-errors";
-import { User } from "../models/user.js";
 import bcrypt from "bcrypt";
-import { createSession, setSessionCookies } from "../services/auth.js";
+import { User } from "../models/user.js";
 import { Session } from "../models/session.js";
+import { createSession, setSessionCookies } from "../services/auth.js";
 
 export const registerUser = async (req, res, next) => {
   const { email, password } = req.body;
 
   const existingUser = await User.findOne({ email });
   if (existingUser) {
-    return next(createHttpError(400, 'Email in use'));
+    return next(createHttpError(400, "Email in use"));
   }
-
 
   const hashedPassword = await bcrypt.hash(password, 10);
   const newUser = await User.create({ email, password: hashedPassword });
@@ -20,7 +19,6 @@ export const registerUser = async (req, res, next) => {
   setSessionCookies(res, session);
 
   res.status(201).json(newUser);
-
 };
 
 export const loginUser = async (req, res, next) => {
@@ -28,61 +26,61 @@ export const loginUser = async (req, res, next) => {
 
   const user = await User.findOne({ email });
   if (!user) {
-    return next(createHttpError(401, 'Invalid credentials'));
+    return next(createHttpError(401, "Invalid credentials"));
   }
 
   const isPasswordValid = await bcrypt.compare(password, user.password);
   if (!isPasswordValid) {
-    return next(createHttpError(401, 'Invalid credentials'));
+    return next(createHttpError(401, "Invalid credentials"));
   }
 
-  await Session.deleteOne({ userId: user._id });
+  await Session.deleteMany({ userId: user._id });
 
-  const newSession = await createSession(user._id);
-  setSessionCookies(res, newSession);
+  const session = await createSession(user._id);
+  setSessionCookies(res, session);
 
   res.status(200).json(user);
 };
 
-export const logoutUser = async (req, res, next) => {
+export const logoutUser = async (req, res) => {
   const { sessionId } = req.cookies;
 
-  if (!sessionId) {
-    return res.status(204).send();
+  if (sessionId) {
+    await Session.deleteOne({ _id: sessionId });
   }
 
-  await Session.deleteOne({ _id: sessionId });
-
-  res.clearCookie('accessToken');
-  res.clearCookie('refreshToken');
-  res.clearCookie('sessionId');
+  res.clearCookie("accessToken");
+  res.clearCookie("refreshToken");
+  res.clearCookie("sessionId");
 
   res.status(204).send();
 };
 
 export const refreshUserSession = async (req, res, next) => {
+  const { sessionId, refreshToken } = req.cookies;
 
   const session = await Session.findOne({
-    _id: req.cookies.sessionId,
-    refreshToken: req.cookies.refreshToken
+    _id: sessionId,
+    refreshToken,
   });
+
   if (!session) {
-    return next(createHttpError(401, 'Session not found'));
+    return next(createHttpError(401, "Session not found"));
   }
 
-  const isSessionTokenExpired = new Date() > new Date(session.refreshTokenValidUntil);
-
-  if (isSessionTokenExpired) {
-    return next(createHttpError(401, 'Session token expired'));
+  if (new Date() > new Date(session.refreshTokenValidUntil)) {
+    await Session.deleteOne({ _id: sessionId });
+    return next(createHttpError(401, "Session token expired"));
   }
 
-  await Session.deleteOne({ _id: req.cookies.sessionId });
+  await Session.deleteOne({ _id: sessionId });
 
   const newSession = await createSession(session.userId);
   setSessionCookies(res, newSession);
 
   res.status(200).json({ message: "Session refreshed" });
 };
+
 
 
 
--- src/controllers/notesController.js
+++ src/controllers/notesController.js
@@ -1,109 +1,93 @@
-import createHttpError from 'http-errors';
-import { Note } from '../models/note.js';
-
+import createHttpError from "http-errors";
+import { Note } from "../models/note.js";
 
 export const getAllNotes = async (req, res) => {
   const { page = 1, perPage = 10, tag, search } = req.query;
   const skip = (page - 1) * perPage;
 
-  const notesQuery = Note.find();
-
-  if (req.user && req.user._id) {
-    notesQuery.where('userId').equals(req.user._id);
-  }
+  const filter = { userId: req.user._id };
 
   if (tag) {
-    notesQuery.where('tag').equals(tag);
+    filter.tag = tag;
   }
 
   if (search) {
-    notesQuery.where({$text: { $search: search }});
+    filter.$text = { $search: search };
   }
 
-  const totalQuery = Note.countDocuments();
-
-  if (req.user && req.user._id) {
-  totalQuery.where('userId').equals(req.user._id);
-}
-
-  if (tag) {
-    totalQuery.where('tag').equals(tag);
-  }
-
-  if (search) {
-    totalQuery.where({$text: { $search: search }});
-  }
   const [notes, total] = await Promise.all([
-    notesQuery.skip(skip).limit(perPage),
-    totalQuery
+    Note.find(filter).skip(skip).limit(perPage),
+    Note.countDocuments(filter),
   ]);
-
 
   const totalPages = Math.ceil(total / perPage);
   if (page > totalPages && totalPages !== 0) {
-    throw createHttpError(400, 'Page number exceeds total pages');
+    throw createHttpError(400, "Page number exceeds total pages");
   }
 
-
-
-  return res.json({
+  res.json({
     page: Number(page),
     perPage: Number(perPage),
     totalNotes: total,
-    totalPages: totalPages,
+    totalPages,
     notes,
   });
 };
 
-export const getNoteById = async (req, res, next) => {
+export const getNoteById = async (req, res) => {
   const { noteId } = req.params;
+
   const note = await Note.findOne({
     _id: noteId,
     userId: req.user._id,
   });
+
   if (!note) {
-    throw createHttpError(404, 'Note not found');
+    throw createHttpError(404, "Note not found");
   }
 
-  res.status(200).json({
-    data: note,
-  });
+  res.status(200).json(note);
 };
 
 export const createNote = async (req, res) => {
-  const note = await Note.create({ ...req.body, userId: req.user._id });
-  res.status(201).json({
-    data: note,
+  const note = await Note.create({
+    ...req.body,
+    userId: req.user._id,
   });
+
+  res.status(201).json(note);
 };
 
 export const deleteNote = async (req, res, next) => {
   const { noteId } = req.params;
+
   const note = await Note.findOneAndDelete({
     _id: noteId,
     userId: req.user._id,
   });
+
   if (!note) {
-    return next(createHttpError(404, 'Note not found'));
+    return next(createHttpError(404, "Note not found"));
   }
-  res.status(200).json({
-    data: note,
-  });
+
+  res.status(200).json(note);
 };
 
 export const updateNote = async (req, res, next) => {
   const { noteId } = req.params;
+
   const note = await Note.findOneAndUpdate(
     { _id: noteId, userId: req.user._id },
     req.body,
     { new: true }
   );
+
   if (!note) {
-    return next(createHttpError(404, 'Note not found'));
+    return next(createHttpError(404, "Note not found"));
   }
-  res.status(200).json({
-    data: note,
-  });
+
+  res.status(200).json(note);
 };
+
 
 

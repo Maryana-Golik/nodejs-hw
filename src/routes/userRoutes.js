45e19d194854bece5ab8dc6c544962457ff4907c
Maryana Golik <golikmaryana307@gmail.com>
2026-02-08 01:41:28 +0200

    fix middleware import paths

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,6 +1,6 @@
 import express from 'express';
 
-import { authenticate } from '../middlewares/authenticate.js';
+import { authenticate } from '../middleware/authenticate.js';
 import { upload } from '../middleware/multer.js';
 import { updateUserAvatar } from '../controllers/userController.js';
 

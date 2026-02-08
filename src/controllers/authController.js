cb3e7bdea073f62b4835284da32551af14066c80
Maryana Golik <golikmaryana307@gmail.com>
2026-02-08 18:12:49 +0200

    finish  05-mail-and-img

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -128,9 +128,11 @@ export const requestResetEmail = async (req, res, next) => {
   const templateSource = await fs.readFile(templatePath, 'utf-8');
   const template = Handlebars.compile(templateSource);
 
-  const html = template({
-    link: `${process.env.FRONTEND_DOMAIN}/reset-password?token=${token}`,
-  });
+ const html = template({
+  name: user.email,
+  link: `${process.env.FRONTEND_DOMAIN}/reset-password?token=${token}`,
+});
+
 
   try {
     await sendEmail({
